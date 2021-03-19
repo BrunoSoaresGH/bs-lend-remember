d37a412d5540bea1040227f4f297f5cb5ab14919
Bruno Soares <brunosoaresbsv@gmail.com>
2021-03-19 18:48:45 -0300

    Project BS Lend Remember created

---
+++ backend/src/main/java/com/bsprojects/lendremember/BslendrememberApplication.java
@@ -0,0 +1,13 @@
+package com.bsprojects.lendremember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BslendrememberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BslendrememberApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bsprojects/lendremember/BslendrememberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsprojects.lendremember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BslendrememberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
